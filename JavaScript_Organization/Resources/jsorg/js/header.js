de291fba546a58dd548c03fb6d4ac4df1338e38b
mark <mark@snapzio.com>
2016-07-06 12:57:38 +0800

    Added JavaScript Organization

---
+++ JavaScript_Organization/Resources/jsorg/js/header.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  var $modal = $("[rel='js-modal']")
+
+  $("[rel='js-control']").on("click","[rel*='js-']",function(evt){
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
+
+    var url = $(evt.target).attr("href");
+
+    $.ajax(url,{ dataType: "text"})
+    .then(function(contents){
+      $modal.html(contents).show();
+    });
+
+  });
+
+});
