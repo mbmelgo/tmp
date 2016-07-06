1be3eb70b5567909929b0f50cc2bf050d6defeba
mark <mark@snapzio.com>
2016-07-06 11:16:05 +0800

    Updated tutorials.js

--- JavaScript_Advanced/tutorials.js
+++ JavaScript_Advanced/tutorials.js
@@ -1,5 +1,20 @@
-for(var i=1; i <=5 ; i++ ){
-  setTimeout(function(){
-      console.log("i: "+i)
+function getData(d){
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){resolve(d);},1000);
   });
 }
+
+var x;
+
+getData(10)
+.then(function(num1){
+  x = 1 + num1;
+  return getData(30);
+})
+.then(function(num2){
+  var y = 1 + num2;
+  return getData("Meaning of life: " + (x+y));
+})
+.then(function(answer){
+    console.log(answer);
+});
